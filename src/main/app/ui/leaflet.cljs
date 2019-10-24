faaebf3c3212c74ca38543647e02181828537bce
bastiion <graviola@posteo.de>
2019-10-24 12:33:09 +0200

    temporary ui changes to allow routing based on several routing methods

--- src/main/app/ui/leaflet.cljs
+++ src/main/app/ui/leaflet.cljs
@@ -3,17 +3,22 @@
     [app.routing.wip :refer [routing-example]]
     [app.ui.leaflet.sidebar :refer [FulcroSidebar fulcroSidebar controlOpenSidebar]]
     [app.ui.leaflet.layers :refer [overlay-class->component]]
+    [app.ui.leaflet.state :as state]
     [app.ui.leaflet.layers.extern.base :refer [baseLayers]]
     [app.ui.leaflet.layers.extern.mvt :refer [mvtLayer]]
     [com.fulcrologic.fulcro.components :refer [defsc factory get-query]]
     [com.fulcrologic.fulcro.algorithms.react-interop :refer [react-factory]]
-    ["react-leaflet" :refer [withLeaflet Map LayersControl LayersControl.Overlay]]
+    ["react-leaflet" :refer [withLeaflet Map LayersControl LayersControl.Overlay Marker Popup]]
+    ["leaflet" :as l]
     [com.fulcrologic.fulcro.dom :as dom]
+    [com.fulcrologic.fulcro.components :as comp]
     [app.model.geofeatures :as gf]))
 
 (def leafletMap (react-factory Map))
 (def layersControl (react-factory LayersControl))
 (def layersControlOverlay (react-factory LayersControl.Overlay))
+(def marker (react-factory Marker))
+(def popup (react-factory Popup))
 
 (defn overlay-filter-rule->filter [filter-rule]
   (if (empty? filter-rule)
@@ -24,9 +29,33 @@
                     filter-rule)
           (reduce #(and %1 %2))))))
 
+(defsc StartStopMarker
+  [this {:keys [lat lng]}]
+  {:query         [:lat :lng]
+   :initial-state {:lat 51 :lng 13}
+   }
+  (marker {:position [lat lng]
+           :icon     (.icon. l (clj->js
+                                 {
+                                  :iconUrl     "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png"
+                                  :shadowUrl   "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png"
+                                  :iconSize    [25, 41]
+                                  :iconAnchor  [12, 41]
+                                  :popupAnchor [1, -34]
+                                  :shadowSize  [41, 41]
+                                  }
+                                 ))}))
+
+(def startStopMarker (factory StartStopMarker {:key-fn #((str (:lat %) (:lng %)))}))
+
 (defsc Leaflet
   [this props]
-  {:query [::gf/id :leaflet/layers]}
+  {:query [:gf/id
+           :leaflet/datasets
+           :leaflet/layers
+           :selected/points
+           :graphhopper/route
+           ]}
   #_(routing-example (get-in props [:leaflet/datasets :vvo :data :geojson]))
 
   (leafletMap {:style {:height "100%" :width "100%"}
@@ -44,7 +73,28 @@
                          component (overlay-class->component (:class overlay))]]
                   (if (and component filtered-features)
                       (component {:react-key (str layer-name (hash overlay) (hash filtered-features))
-                                  :geojson {:type "FeatureCollection" :features filtered-features}}))))) )))
+                                  :geojson {:type "FeatureCollection" :features filtered-features}})))))
+      (layersControlOverlay
+        {:key "test-x-layer" :name "test" :checked true}
+        #_(marker {:position [51.055 13.74]
+                   :icons (.icon. l (clj->js
+                                      {
+                                       :iconUrl     "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png"
+                                       :shadowUrl   "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png"
+                                       :iconSize    [25, 41]
+                                       :iconAnchor  [12, 41]
+                                       :popupAnchor [1, -34]
+                                       :shadowSize  [41, 41]
+                                       }
+                                      ))
+                   })
+        (if (nil? (:selected/points props))
+          []
+          (for [point (:selected/points props)]
+            (startStopMarker (:selected/latlng point))
+            )
+          )
+        ))))
 
 (def leaflet (factory Leaflet))
 
@@ -54,6 +104,6 @@
   (dom/div {:style {:width "100%" :height "100%"}}
     (if (get-in props [:leaflet/sidebar :visible])
         (fulcroSidebar props))
-    (leaflet (select-keys props [::gf/id :leaflet/layers]))))
+    (leaflet (select-keys props [:leaflet/datasets :leaflet/layers :selected/points]))))
 
 (def leafletWithSidebar (factory LeafletWithSidebar))
