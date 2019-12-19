2bcd3a0018e5378c7a667d3d85634d7a3f4c4ee5
bastiion <graviola@posteo.de>
2019-12-19 10:58:31 +0100

    client and server site background tracking (GPX feature)

---
+++ src/main/app/background_geolocation.clj
@@ -0,0 +1,71 @@
+(ns app.background-geolocation
+  (:require [com.wsscode.pathom.connect :as pc]
+            [com.wsscode.pathom.core :as p]
+            [com.wsscode.common.async-clj :refer [let-chan]]
+            [clojure.core.async :as async]
+            [app.server-components.config :refer [config]]
+            [clojure.spec.gen.alpha :as sgen]
+            [clojure.spec.alpha :as s]
+            [clj-time.core :as time]
+            [clj-time.coerce :as tc]))
+
+(s/def ::latitude double?)
+(s/def ::longitude double?)
+(s/def ::accuracy pos-int?)
+(s/def ::speed double?)
+(s/def ::heading pos-int?)
+(s/def ::altitude pos-int?)
+
+(s/def ::coords (s/keys ::latitude ::longitude ::accuracy ::speed ::heading ::altitude))
+
+(s/def ::is_moving boolean?)
+(s/def ::odometer double?)
+(s/def ::uuid string?)
+(s/def ::timestamp inst?)
+(s/valid? ::timestamp inst? (java.util.Date.))
+(s/def ::event string?)
+
+(s/def ::pos (s/keys :req [::coords ::is_moving ::odometer ::uid ::timestamp]))
+
+(pc/defresolver latest-track [_ _]
+  {::pc/output [{::latest-track [::uuid ::coords ::timestamp]}]}
+  {::latest-track {::uiid      "102312321"
+                   ::coords    {}
+                   ::timestamp (tc/to-sql-time (time/now))}})
+
+(pc/defresolver save-gpx-track [env {:background-location/keys [track]}]
+  {
+   ::pc/params [:background-location/track]
+   ::pc/output [:message/id :message/text]}
+  (do
+    (prn "server mutation called")
+    {:message/id   123
+     :message/text track}))
+
+(pc/defmutation send-message [env {:keys [message/text]}]
+  {::pc/sym    'send-message
+   ::pc/params [:message/text]
+   ::pc/output [:message/id :message/text]}
+  {:message/id   123
+   :message/text text})
+
+(def parser
+  (p/parser
+    {::p/env     {::p/reader               [p/map-reader
+                                            pc/reader2
+                                            pc/open-ident-reader
+                                            p/env-placeholder-reader]
+                  ::p/placeholder-prefixes #{">"}}
+     ::p/mutate  pc/mutate
+     ::p/plugins [(pc/connect-plugin {::pc/register [latest-track save-gpx-track send-message]})
+                  p/error-handler-plugin
+                  p/trace-plugin]}))
+
+(comment
+  (parser {} ['(save-gpx-track {:background-location/track {::coords []}})])
+
+
+  (parser {} ['(send-message {:message/text "Hello Clojurist!"})])
+  )
+
+
--- src/main/app/background_geolocation.cljs
+++ src/main/app/background_geolocation.cljs
@@ -1,101 +1,141 @@
 (ns app.background-geolocation
   (:require [com.fulcrologic.fulcro.components :as comp]
+            [com.fulcrologic.fulcro.mutations :refer [defmutation]]
             [app.application :refer [SPA]]
+            [com.fulcrologic.fulcro.ui-state-machines :as uism]
             [app.ui.leaflet.state :refer [new-sensor-data new-location-data]]
             ))
 
 
 #_(def bg-properties [
-    "CY_MEDIUM"
-    "DESIRED_ACCURACY_LOW"
-    "DESIRED_ACCURACY_VERY_LOW"
-    "DESIRED_ACCURACY_THREE_KILOMETER"
-    "AUTHORIZATION_STATUS_NOT_DETERMINED"
-    "AUTHORIZATION_STATUS_RESTRICTED"
-    "AUTHORIZATION_STATUS_DENIED"
-    "AUTHORIZATION_STATUS_ALWAYS"
-    "AUTHORIZATION_STATUS_WHEN_IN_USE"
-    "NOTIFICATION_PRIORITY_DEFAULT"
-    "NOTIFICATION_PRIORITY_HIGH"
-    "NOTIFICATION_PRIORITY_LOW"
-    "NOTIFICATION_PRIORITY_MAX"
-    "NOTIFICATION_PRIORITY_MIN"
-    "ACTIVITY_TYPE_OTHER"
-    "ACTIVITY_TYPE_AUTOMOTIVE_NAVIGATION"
-    "ACTIVITY_TYPE_FITNESS"
-    "ACTIVITY_TYPE_OTHER_NAVIGATION"
-    "PERSIST_MODE_ALL"
-    "PERSIST_MODE_LOCATION"
-    "PERSIST_MODE_GEOFENCE"
-    "PERSIST_MODE_NONE"
-    "deviceSettings"
-    "logger"
-    "ready"
-    "configure"
-    "reset"
-    "requestPermission"
-    "getProviderState"
-    "onLocation"
-    "onMotionChange"
-    "onHttp"
-    "onHeartbeat"
-    "onProviderChange"
-    "onActivityChange"
-    "onGeofence"
-    "onGeofencesChange"
-    "onSchedule"
-    "onEnabledChange"
-    "onConnectivityChange"
-    "onPowerSaveChange"
-    "onNotificationAction"
-    "on"
-    "un"
-    "removeListener"
-    "removeListeners"
-    "getState"
-    "start"
-    "stop"
-    "startSchedule"
-    "stopSchedule"
-    "startGeofences"
-    "startBackgroundTask"
-    "stopBackgroundTask"
-    "finish"
-    "changePace"
-    "setConfig"
-    "getLocations"
-    "getCount"
-    "destroyLocations"
-    "clearDatabase"
-    "insertLocation"
-    "sync"
-    "getOdometer"
-    "resetOdometer"
-    "setOdometer"
-    "addGeofence"
-    "removeGeofence"
-    "addGeofences"
-    "removeGeofences"
-    "getGeofences"
-    "getGeofence"
-    "geofenceExists"
-    "getCurrentPosition"
-    "watchPosition"
-    "stopWatchPosition"
-    "registerHeadlessTask"
-    "setLogLevel"
-    "getLog"
-    "destroyLog"
-    "emailLog"
-    "isPowerSaveMode"
-    "getSensors"
-    "playSound"
-    "transistorTrackerParams"
-    "test"])
+                      "CY_MEDIUM"
+                      "DESIRED_ACCURACY_LOW"
+                      "DESIRED_ACCURACY_VERY_LOW"
+                      "DESIRED_ACCURACY_THREE_KILOMETER"
+                      "AUTHORIZATION_STATUS_NOT_DETERMINED"
+                      "AUTHORIZATION_STATUS_RESTRICTED"
+                      "AUTHORIZATION_STATUS_DENIED"
+                      "AUTHORIZATION_STATUS_ALWAYS"
+                      "AUTHORIZATION_STATUS_WHEN_IN_USE"
+                      "NOTIFICATION_PRIORITY_DEFAULT"
+                      "NOTIFICATION_PRIORITY_HIGH"
+                      "NOTIFICATION_PRIORITY_LOW"
+                      "NOTIFICATION_PRIORITY_MAX"
+                      "NOTIFICATION_PRIORITY_MIN"
+                      "ACTIVITY_TYPE_OTHER"
+                      "ACTIVITY_TYPE_AUTOMOTIVE_NAVIGATION"
+                      "ACTIVITY_TYPE_FITNESS"
+                      "ACTIVITY_TYPE_OTHER_NAVIGATION"
+                      "PERSIST_MODE_ALL"
+                      "PERSIST_MODE_LOCATION"
+                      "PERSIST_MODE_GEOFENCE"
+                      "PERSIST_MODE_NONE"
+                      "deviceSettings"
+                      "logger"
+                      "ready"
+                      "configure"
+                      "reset"
+                      "requestPermission"
+                      "getProviderState"
+                      "onLocation"
+                      "onMotionChange"
+                      "onHttp"
+                      "onHeartbeat"
+                      "onProviderChange"
+                      "onActivityChange"
+                      "onGeofence"
+                      "onGeofencesChange"
+                      "onSchedule"
+                      "onEnabledChange"
+                      "onConnectivityChange"
+                      "onPowerSaveChange"
+                      "onNotificationAction"
+                      "on"
+                      "un"
+                      "removeListener"
+                      "removeListeners"
+                      "getState"
+                      "start"
+                      "stop"
+                      "startSchedule"
+                      "stopSchedule"
+                      "startGeofences"
+                      "startBackgroundTask"
+                      "stopBackgroundTask"
+                      "finish"
+                      "changePace"
+                      "setConfig"
+                      "getLocations"
+                      "getCount"
+                      "destroyLocations"
+                      "clearDatabase"
+                      "insertLocation"
+                      "sync"
+                      "getOdometer"
+                      "resetOdometer"
+                      "setOdometer"
+                      "addGeofence"
+                      "removeGeofence"
+                      "addGeofences"
+                      "removeGeofences"
+                      "getGeofences"
+                      "getGeofence"
+                      "geofenceExists"
+                      "getCurrentPosition"
+                      "watchPosition"
+                      "stopWatchPosition"
+                      "registerHeadlessTask"
+                      "setLogLevel"
+                      "getLog"
+                      "destroyLog"
+                      "emailLog"
+                      "isPowerSaveMode"
+                      "getSensors"
+                      "playSound"
+                      "transistorTrackerParams"
+                      "test"])
 
 (def bg js/window.BackgroundGeolocation)
 
 
+(defmutation save-gpx-track [{:background-location/keys [track]}]
+  (pathom [{:keys [ast]}]
+          (do (prn ast)
+              (let [_track (seq track)]
+                (update ast :params select-keys [:background-location/track])))))
+
+(defmutation update-background-location-tracking-state [{:keys [bgstate]}]
+  (action [{:keys [state]}]
+          (do
+            (swap! state into {:background-location/state bgstate})
+            (bg.getLocations
+              (fn [locations]
+                (let [track (js->clj locations :keywordize-keys true)]
+                  (prn "Will save gpx track (mutation)")
+                  (comp/transact!
+                    SPA
+                    [(save-gpx-track {:background-location/track  track})]
+                    )))))))
+
+(defn bg-state-update! []
+  (.getState
+    bg
+    (fn [state]
+      (comp/transact!
+        SPA
+        [(update-background-location-tracking-state {:bgstate (js->clj state :keywordize-keys true)})]))))
+
+(defmutation start-tracking [_]
+  (action [{:keys [state]}]
+          (prn "Will enable")
+          (bg.start
+            bg-state-update!)))
+
+(defmutation stop-tracking [_]
+  (action [{:keys [state]}]
+          (prn "Will stop")
+          (bg.stop
+            bg-state-update!)))
 
 (defn bg-ready!
   []
@@ -107,24 +147,35 @@
               :stopTimeout       1
               :distanceFilter    10
               :desiredAccuracy   bg.DESIRED_ACCURACY_HIGH
+              :url               "http://10.0.2.2:9001/locations"
+              :autoSync          true
               ;:params (.transistorTrackerParams bg js/window.device)
               :stopOnTerminate   false
               :startOnBoot       true
               :foregroundService true}
-      ),
+             ),
     (fn [state]
-      (js/console.log "success " state)
-      )
-    )
-  )
+      (comp/transact! SPA [(update-background-location-tracking-state {:bgstate (js->clj state :keywordize-keys true)})])
+      )))
 
 (defn listenForEvents!
   []
   (.onLocation
     bg
     (fn [location]
-      #(js/console.log "location" location)
       (comp/transact! SPA [(new-location-data {:values (js->clj location :keywordize-keys true) :sensor_type "LOCATION"})]))
+    )
+  (.onEnabledChange
+    bg
+    (fn [isEnabled]
+      bg-state-update!
+      )
+    )
+  (.onActivityChange
+    bg
+    (fn [event]
+      (comp/transact! SPA [(new-location-data {:value (js->clj event :keywordize-keys true) :sensor_type "ACTIVITY"})])
+      )
     )
   (.onMotionChange
     bg
@@ -150,13 +201,11 @@
 
 
 (defn bg-prepare!
+  "initializes the background geo location plugin, if the plugin has been loaded in a cordova environment
+  and js/window.BackgroundLocation is set globally"
   []
   (when (some? bg)
     (listenForEvents!)
     (bg-ready!)
-    (intervalLocation 8000)
-
-    #_(.addEventListener js/document "deviceready"
-                       #(do
-                          (listenForEvents!)
-                          (bg-ready!)))))
+    #_(intervalLocation 8000)
+    ))
