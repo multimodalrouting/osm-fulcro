5da275972f4209721c7f10fd89d96a1cfb00a14d
bastiion <graviola@posteo.de>
2020-01-30 13:26:43 +0100

    circle ci karma with no-sandbox for ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,26 @@
+const isDocker = require('is-docker')();
+
 module.exports = function (config) {
-    config.set({
-        browsers: ['ChromeHeadless'],
-        // The directory where the output file lives
-        basePath: 'target',
-        // The file itself
-        files: ['ci.js'],
-        frameworks: ['cljs-test'],
-        plugins: ['karma-cljs-test', 'karma-chrome-launcher'],
-        colors: true,
-        logLevel: config.LOG_INFO,
-        client: {
-            args: ["shadow.test.karma.init"],
-            singleRun: true
-        }
-    })
+  config.set({
+    customLaunchers: {
+      ChromeCustom: {
+        base: 'ChromeHeadless',
+        // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+        // more permissions than Docker allows by default)
+        flags: isDocker ? ['--no-sandbox'] : []
+      }
+    },
+    // The directory where the output file lives
+    basePath: 'target',
+    // The file itself
+    files: ['ci.js'],
+    frameworks: ['cljs-test'],
+    plugins: ['karma-cljs-test', 'karma-chrome-launcher'],
+    colors: true,
+    logLevel: config.LOG_INFO,
+    client: {
+      args: ["shadow.test.karma.init"],
+      singleRun: true
+    }
+  })
 };
