267dc9aa7adb9198e7ae7943a980c6ae87a11d4b
bastiion <graviola@posteo.de>
2020-02-18 02:46:36 +0100

    support labled points and striped patterns based on geojson features

--- src/main/app/ui/leaflet/d3.cljs
+++ src/main/app/ui/leaflet/d3.cljs
@@ -11,18 +11,47 @@
   (.latLngToLayerPoint proj (clj->js {:lat lat :lng lng})))
 
 (defn bounds->circumcircleRadius [proj bounds]
-   (some->> bounds
-            (map (partial lngLat->Point proj))
-            (#(.subtract (first %) (second %)))
-            (#(js/Math.sqrt (+ (* (.-x %) (.-x %))
-                               (* (.-y %) (.-y %)))))
-            (* 0.5)))
+  (some->> bounds
+           (map (partial lngLat->Point proj))
+           (#(.subtract (first %) (second %)))
+           (#(js/Math.sqrt (+ (* (.-x %) (.-x %))
+                              (* (.-y %) (.-y %)))))
+           (* 0.5)))
 
+
+(defn stripe-pattern [defs id size color]
+  "generates a stripe pattern as in the given defs
+  it can be referenced using url(#id) in a shape, where
+  id is the concrete id given here
+  "
+  (-> defs
+      (.append "pattern")
+      (.attr "id" id)
+      (.attr "width" size)
+      (.attr "height" size)
+      (.attr "patternUnits" "userSpaceOnUse")
+      (.attr "patternTransform" "rotate(60)")
+      (.append "rect")
+      (.attr "width" (/ size 2))
+      (.attr "height" size)
+      (.attr "transform" "translate(0,0)")
+      (.attr "fill" color)))
+
+(defn accessibility-patterns [svg size]
+  (let [defs (.append svg "defs")]
+    (for [[name color]
+          [["yes" "#7ED321"]
+           ["no" "#D0021B"]
+           ["limited" "#F5A623"]
+           ["other" "grey"]]]
+      (stripe-pattern defs name size color)
+      )))
 
 (defn color-by-accessibility [d]
-  ({"yes" "green"
-    "no" "red"
-    "limited" "yellow"}
+  ({"yes"     "#7ED321"                                     ;green
+    "no"      "#D0021B"                                     ;red
+    "limited" "#F5A623"                                     ;orange
+    }
    (get-in (js->clj d :keywordize-keys true)
            [:properties :wheelchair])
-   "blue"))
+   "grey"))
---
+++ src/main/app/ui/leaflet/layers/d3svg_label_points.cljs
@@ -0,0 +1,57 @@
+(ns app.ui.leaflet.layers.d3svg-label-points
+  (:require
+    [com.fulcrologic.fulcro.components :refer [defsc]]
+    [app.ui.leaflet.d3 :refer [d3SvgOverlay lngLat->Point color-by-accessibility accessibility-patterns]]))
+
+
+(defn feature->confident? [feature]
+  (if-let [confidence (get-in feature [:properties :confidence])]
+     (>= confidence 0.5)))
+
+(defn d3DrawCallback [sel proj data]
+  (let [radius 3
+        x-fn (fn [d] (.-x (lngLat->Point proj (get-in (js->clj d :keywordize-keys true) [:geometry :coordinates]))))
+        y-fn (fn [d] (.-y (lngLat->Point proj (get-in (js->clj d :keywordize-keys true) [:geometry :coordinates]))))
+        svg (-> sel
+              (.select (fn [] (this-as self
+                                (js/console.log self)
+                                       self.parentNode)))
+
+                    )
+        pattern (dorun (accessibility-patterns svg 2))
+        upd (-> sel
+                (.selectAll "a")
+                (.data (clj->js data)))
+        a-el (->
+               (.enter upd)
+               (.append "a")
+               (.on "click" (fn [d i ds] (js/console.log (js->clj d))))
+               )
+        circle-el (-> a-el
+                      (.append "circle")
+                      (.attr "cx" x-fn)
+                      (.attr "cy" y-fn)
+                      (.attr "r" 4)
+                      (.attr "fill" #(let [feature (js->clj % :keywordize-keys true)]
+                                          (if (feature->confident? feature)
+                                            (str "url(#" (get-in feature [:properties :wheelchair]) ")")
+                                            (color-by-accessibility feature))
+                                          ) )
+                      (.attr "fill-opacity" 0.9)
+                    )
+        text-el (-> a-el
+                    (.append "text")
+                    (.text #(get-in (js->clj % :keywordize-keys true) [:properties :symbol-text] ))
+                    (.style "font", "bold 8px sans-serif")
+                    (.attr "x" (fn [d] (- (x-fn d) radius)))
+                    (.attr "y" (fn [d] (+ (y-fn d) radius)))
+                    )
+
+        ]
+     a-el
+    ))
+
+(defsc D3SvgLabelPoints [this {:keys [react-key geojson]}]
+  (d3SvgOverlay {:key react-key
+                 :data (:features geojson)
+                 :drawCallback d3DrawCallback}))
